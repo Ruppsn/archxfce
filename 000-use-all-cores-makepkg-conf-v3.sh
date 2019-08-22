87f1f19fd7208d79b505756be2e2d8b5e232bf63
Philipp Ru <Ruppertus98@gmail.com>
2019-08-22 10:02:41 +0200

    added more comments

--- 000-use-all-cores-makepkg-conf-v3.sh
+++ 000-use-all-cores-makepkg-conf-v3.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+# exit at fail
 set -e
+
+
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
+# Script is used to activate all cores while building and compression.
+
 
 numberofcores=$(grep -c ^processor /proc/cpuinfo)
 

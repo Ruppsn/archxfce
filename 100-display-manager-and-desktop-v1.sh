fec29db0f7bca3d45bd13cf7528454d55b168eb4
Philipp Ru <Ruppertus98@gmail.com>
2019-08-22 10:06:17 +0200

    make it pretty

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 set -e
+
+
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
+# Script for installing the display manager for arch
+
 echo "#################################################"
 echo "If it feels like the downloads are too slow"
 echo "Stop the installation with CTRL + C"
 echo "and run the alias - mirror in the terminal"
 echo "#################################################"
 
+
 sudo pacman -Syyu --noconfirm
-#installing displaymanager or login manager
+
+
+echo "#################################################"
+echo "Installing the display manager"
+echo "#################################################"
 sudo pacman -S --noconfirm --needed lightdm
 sudo pacman -S --noconfirm --needed arcolinux-lightdm-gtk-greeter arcolinux-lightdm-gtk-greeter-settings
 sudo pacman -S --noconfirm --needed arcolinux-wallpapers-git
-#installing desktop environment
+
+
+echo "#################################################"
+echo "Installing the desktop env"
+echo "#################################################"
 sudo pacman -S xfce4 xfce4-goodies --noconfirm --needed
-#enabling displaymanager or login manager
+
+
+echo "#################################################"
+echo "Start and enable the display manager and set as default"
+echo "#################################################"
 sudo systemctl enable lightdm.service -f
 sudo systemctl set-default graphical.target
 
 
-#Remove anything you do not like from the installed applications
 
-#sudo pacman -R ...
+echo "#################################################"
+echo "Removing contend I don't want"
+echo "#################################################"
 sudo pacman -R xfce4-artwork --noconfirm
